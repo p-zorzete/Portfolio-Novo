f23932ccecb2e05d99af8a6a1a62a00253ca1837
Pedro Zorzete <pedro.zorzeteb@gmail.com>
2024-10-09 00:45:46 -0300

    Comecei a escrever o css, começando pelo componente capa

--- src/Componentes/Capa.js
+++ src/Componentes/Capa.js
@@ -1,44 +1,49 @@
 import { Link } from "react-router-dom";
+import canto from "../Assets/Canto.png"
 
 function Capa() {
       return (
             <div>
-                  <div className="apresentacao">
-                        <h1 className="titulo">Bem Vindo</h1>
-                        <h2 className="resumo">Meu nome é Pedro Zorzete. Tenho 23 anos e sou um estudante de TI com foco em programação e desenvolvimento, estudo o back e o front possuindo conhecimento nessas duas partes do desenvolvimento.</h2>
+                  <div id="capa">
+                        <div id="apresentacao">
+                              <h1 className="titulo">Bem Vindo</h1>
+                              <h2 className="resumo">Meu nome é Pedro Zorzete. Tenho 23 anos e sou um estudante de TI com foco em programação e desenvolvimento, estudo o back e o front possuindo conhecimento nessas duas partes do desenvolvimento.</h2>
+                        </div>
+                        <nav id="menuLateral">
+                              <h3 id="textoMenu">Menu</h3>
+                              <img className="cantos" id="cantoSuperior" src={canto}/>
+                              <ul id="listaMenu">
+                                    <li className="itemMenu">
+                                          <Link to="#sobre">
+                                                <span class="linkTexto">Sobre mim</span>
+                                          </Link>
+                                    </li>
+                                    <li className="itemMenu">
+                                          <Link to="#linguagens">
+                                                <span className="linkTexto">Linguagens</span>
+                                          </Link>
+                                    </li>
+                                    <li className="itemMenu">
+                                          <Link to="#habilidades">
+                                                <span className="linkTexto">Habilidades</span>
+                                          </Link>
+                                    </li>
+                                    <li className="itemMenu">
+                                          <Link to="#projetos">
+                                                <span className="linkTexto">Projetos</span>
+                                          </Link>
+                                    </li>
+                                    <li className="itemMenu">
+                                          <Link to="#contato">
+                                                <span className="linkTexto">Contato</span>
+                                          </Link>
+                                    </li>
+                              </ul>
+                              <img className="cantos" id="cantoInferior" src={canto}/>
+                        </nav>
                   </div>
-                  <nav className="menuLateral">
-                        <h3 className="textoMenu">Menu</h3>
-                        <ul>
-                              <li>
-                                    <Link to="#sobre">
-                                        <span class="linkTexto">Sobre mim</span>
-                                    </Link>
-                              </li>
-                              <li>
-                                    <Link to="#linguagens">
-                                        <span class="linkTexto">Linguagens</span>
-                                    </Link>
-                              </li>
-                              <li>
-                                    <Link to="#habilidades">
-                                        <span class="linkTexto">Habilidades</span>
-                                    </Link>
-                              </li>
-                              <li>
-                                    <Link to="#projetos">
-                                        <span class="linkTexto">Projetos</span>
-                                    </Link>
-                              </li>
-                              <li>
-                                    <Link to="#contato">
-                                        <span class="linkTexto">Contato</span>
-                                    </Link>
-                              </li>
-                        </ul>
-                  </nav>
-                  <div className="blocoInferior">
-                        <p>Meus projetos e informações</p>
+                  <div id="blocoInferior">
+                              <p>Meus projetos e informações</p>
                   </div>
             </div>
       )
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,6 +4,7 @@ import Habilidades from "../Componentes/Habilidades";
 import Linguagens from "../Componentes/Linguagens";
 import Projetos from "../Componentes/Projetos";
 import Sobre from "../Componentes/Sobre";
+import "../Css/style.css"
 
 function Home() {
       return (
